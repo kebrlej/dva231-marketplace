1b73bc7432d501fc4f9c8a0509202ef864073bc6
Jan Kebrle <s.kebrle@gmail.com>
2019-11-01 11:10:20 +0100

    Local storage created

---
+++ public/js/localStorage.js
@@ -0,0 +1,20 @@
+
+
+function saveToStorage(key, value){
+
+}
+
+function getFromStorage(key, value){
+
+}
+
+
+var recentlyVisitedName = "";
+
+function addRecentlyVisitedProduct(id, title){
+
+
+    getFromStorage("recentlyVisitied")
+
+}
+
