3f569cdda63f4097d6b4f1d4c638050a5f5acfa4
Jan Kebrle <s.kebrle@gmail.com>
2019-11-04 16:56:57 +0100

    Cross site request warning fixed.

--- public/index.php
+++ public/index.php
@@ -60,7 +60,7 @@ if (isset($_GET['page'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="css/w3.css" type="text/css">
 <!--    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>-->
     <link href="css/mainstyle.css" rel="stylesheet" type="text/css"/>
 
