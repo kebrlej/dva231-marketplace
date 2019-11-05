adc5f25d94527bf4867815ee0981d8e42c36c88f
Jan Kebrle <s.kebrle@gmail.com>
2019-11-05 11:41:44 +0100

    Javascript error fix

--- public/js/historyBox.js
+++ public/js/historyBox.js
@@ -1,9 +1,9 @@
 function populateHistoryBox() {
-    let historyBox = document.getElementById("historyBoxLinks");
-    getRecentlyVisitedProducts().forEach(function(recentlyVisitedProduct){
-       var element = document.createElement("a");
-       element.onclick= "displayProduct("+recentlyVisitedProduct.id+")";
-       element.innerText = recentlyVisitedProduct.title;
-       historyBox.appendChild(element);
-    });
+    // let historyBox = document.getElementById("historyBoxLinks");
+    // getRecentlyVisitedProducts().forEach(function(recentlyVisitedProduct){
+    //    var element = document.createElement("a");
+    //    element.onclick= "displayProduct("+recentlyVisitedProduct.id+")";
+    //    element.innerText = recentlyVisitedProduct.title;
+    //    historyBox.appendChild(element);
+    // });
 }
